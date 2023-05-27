b01b66c3606e83147f7c6961d25f550ff2b0b04b
OPADILOP <86042583+OPADILOP@users.noreply.github.com>
2023-05-27 14:17:34 +0600

    Update helpCommand.java

--- src/main/java/ga/pixelplayz/hideplayer/hideplayer/commands/subcommands/helpCommand.java
+++ src/main/java/ga/pixelplayz/hideplayer/hideplayer/commands/subcommands/helpCommand.java
@@ -5,6 +5,6 @@ import org.bukkit.command.CommandSender;
 
 public class helpCommand {
     public void onHelp(CommandSender sender){
-        sender.sendMessage(ChatColor.DARK_GRAY+"------------[--: "+ChatColor.GREEN+"HidePlayer: "+ChatColor.DARK_GRAY+"--]------------\n   "+ChatColor.YELLOW+"/help: "+ChatColor.GRAY+"- Display This Menu\n    "+ChatColor.YELLOW+"/hide: "+ChatColor.GRAY+"- Hide All Players\n    "+ChatColor.YELLOW+"/show: "+ChatColor.GRAY+"- Show All Players\n    "+ChatColor.YELLOW+"/reload: "+ChatColor.GRAY+"- Reload The Plugin\n "+ChatColor.DARK_GRAY+"--------------------------------------------");
+        sender.sendMessage(ChatColor.DARK_GRAY+"------------[--: "+ChatColor.GREEN+"HidePlayer "+ChatColor.DARK_GRAY+":--]------------\n   "+ChatColor.YELLOW+"/help "+ChatColor.GRAY+"- Display This Menu\n    "+ChatColor.YELLOW+"/hide "+ChatColor.GRAY+"- Hide All Players\n    "+ChatColor.YELLOW+"/show "+ChatColor.GRAY+"- Show All Players\n    "+ChatColor.YELLOW+"/reload "+ChatColor.GRAY+"- Reload The Plugin\n "+ChatColor.DARK_GRAY+"--------------------------------------------");
     }
 }
